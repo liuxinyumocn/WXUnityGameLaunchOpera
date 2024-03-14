c58d0da403f094beeb1a842f72b31dc6cc502da9
nebulaliu <nebulaliu@tencent.com>
2024-03-14 20:09:03 +0800

    feat: cli

--- cli/main.ts
+++ cli/main.ts
@@ -1,6 +1,16 @@
 import main from '@main/';
+import * as fs from 'fs';
+const path = require('path');
 const md5 = require('md5');
-console.log(JSON.stringify({
+const content = JSON.stringify({
   target: 'lacOpera',
   out: main.exportData({}, md5),
-}));
+});
+const root = process.cwd();
+fs.writeFileSync(
+  path.resolve(root, 'dist', 'temp.t'),
+  content,
+  {
+    encoding: 'utf-8'
+  }
+);
--- webpack.config.js
+++ webpack.config.js
@@ -24,5 +24,5 @@ module.exports = {
     ]
   },
   plugins: [],
-  // watch: true
+  target: 'node',
 };
